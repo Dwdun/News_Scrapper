2af0a5f4db89a913a237c6a7f72dec5ad2212abe
iirfanr <irfan.ramadhan021006@gmail.com>
2026-03-05 21:19:18 +0700

    added file & console handler

--- Utils(faqih,anin,irfan)/logger.py
+++ Utils(faqih,anin,irfan)/logger.py
@@ -2,7 +2,7 @@ import logging
 import logging.handlers
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOG_DIR = os.path.join(BASE_DIR, "Logs(irfan)")
 os.makedirs(LOG_DIR, exist_ok=True)
 
@@ -14,3 +14,15 @@ if not logger.handlers:
         "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
         )
 
+log_file_path = os.path.join(LOG_DIR, "scraper.log")
+
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
