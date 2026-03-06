0ffc005a51f9e86b14debf12898e43b1b2c871b5
Bima Anugrah Prasetyo <bimabetter.335@gmail.com>
2026-03-06 14:37:10 +0700

    Setup Selenium Scraper

---
+++ Scrapper(bima)/selenium_scrapper.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import json
+import os
+import time
+def setup_driver():
+    """Konfigurasi dan return WebDriver untuk Brave Browser."""
+    options = Options()
+    options.binary_location = '/usr/bin/brave-browser'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
+    return driver
+if __name__ == '__main__':
+    driver = setup_driver()
+    print("Browser berhasil dijalankan!")
+    driver.quit()
