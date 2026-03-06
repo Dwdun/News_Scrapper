e98aae1c01422695740bc7cc410973eb4a380b78
Bima Anugrah Prasetyo <bimabetter.335@gmail.com>
2026-03-06 16:46:43 +0700

    Menambahkan article extractor dan memperbaiki selenium scrapper

--- scrapper/selenium_scrapper.py
+++ scrapper/selenium_scrapper.py
@@ -10,7 +10,6 @@ def setup_driver():
     import shutil
     
     options = Options()
-    options.binary_location = '/usr/bin/brave-browser'
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
