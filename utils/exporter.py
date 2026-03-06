c859975cb6dc1f2e2e11eee77bafe02c57074e0f
aniinn <anindya.jauzaa.tif425@polban.ac.id>
2026-03-06 19:18:44 +0700

    fungsi export to excel, style

--- utils/exporter.py
+++ utils/exporter.py
@@ -1,4 +1,6 @@
 import csv
+import openpyxl
+from openpyxl.styles import Font, PatternFill, Alignment
 
 # FIELDS: bagian data artikel yang mau dimasukkan ke file
 FIELDS = ["title", "date", "content", "url"]
@@ -29,4 +31,35 @@ def export_to_csv(data, filepath):
     
     except Exception:
         # Jika ada file yang gagal diekspor (error)
+        return False
+
+def export_to_excel(data, filepath):
+    """
+    Mengekspor daftar artikel ke dalam file format Excel
+    """
+    try:
+        # Membuat file Excel baru
+        wb = openpyxl.Workbook()
+        
+        # Mengambil sheet Excel yang akan kita isi data
+        ws = wb.active
+        
+        # Mengganti nama sheet
+        ws.title = "Hasil Scraping"
+        
+        # Menulis baris pertama sebagai header, tambahkan awalan kolom "No"
+        ws.append(["No"] + HEADERS)
+        
+        # Mengatur tampilan header
+        header_font = Font(bold=True, color="FFFFFF")
+        header_fill = PatternFill(start_color="0070C0", end_color="0070C0", fill_type="solid")
+        header_alignment = Alignment(horizontal="center", vertical="center")
+        
+        # Menerapkan tampilan tersebut ke semua cell di baris pertama
+        for cell in ws[1]:
+            cell.font = header_font
+            cell.fill = header_fill
+            cell.alignment = header_alignment
+        
+    except Exception:
         return False
